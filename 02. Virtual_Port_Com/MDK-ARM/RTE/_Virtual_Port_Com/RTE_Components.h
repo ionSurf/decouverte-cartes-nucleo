8bb43a75f19c1049890363bcb09b24456e0e6bab
ionSurf <javier.acounts@fiml.co>
2020-10-04 21:48:15 -0400

    cleaning

---
+++ 02. Virtual_Port_Com/MDK-ARM/RTE/_Virtual_Port_Com/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Virtual_Port_Com' 
+ * Target:  'Virtual_Port_Com' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
